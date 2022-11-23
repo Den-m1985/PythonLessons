4e48c0b11bb1b423b28c7c34cd47728dd4666a72
den-m1985 <denismartyanov244@gmail.com>
2022-11-23 07:22:07 +0200

    add method task2

--- Home_Work_1/Ex_2.py
+++ Home_Work_1/Ex_2.py
@@ -1,19 +1,18 @@
+
 '''
 Напишите программу для
 проверки истинности утверждения 
 ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-
-
 '''
-X = 1
-Y = 1
-Z = 1
 
-left = not (X or Y or Z)
-right = not X and not Y and not Z
+def Calculate (X, Y, Z):
+   left = not (X or Y or Z)
+   right = not X and not Y and not Z
 
-if left == right:
-   print("Равенство равно")
-else:
-    print("Равенство не равно")
+   if left == right:
+      print("Равенство равно")
+   else:
+      print("Равенство не равно")
+
+Calculate(1, 1, 1)
 
