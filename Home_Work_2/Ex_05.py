57529d25ac775b7f7d7c273180b4fa030ef53fdb
den-m1985 <denismartyanov244@gmail.com>
2022-11-25 15:13:21 +0200

    add solution task 5

--- Home_Work_2/Ex_05.py
+++ Home_Work_2/Ex_05.py
@@ -1,5 +1,27 @@
+from random import randint
 '''
 5 Реализуйте алгоритм перемешивания списка.
 Из библиотеки random использовать можно только randint
 '''
 
+
+def fill_Array(number):
+    lst = []
+    for item in range(0, number):
+        lst.append(item)
+    return lst
+
+
+def sort(array, num):
+    lst = []
+    temp = 0
+    for i in array:
+        i = randint(0, num-1)
+        lst.append(array[i])
+    return lst
+
+
+num = 10   # длина списка
+array = fill_Array(num)
+print(array)
+print(sort(array, num))
