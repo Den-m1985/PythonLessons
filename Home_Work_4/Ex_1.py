3f711aef29f163e405e5d7061cb9b18a6ddf4f1c
den-m1985 <denismartyanov244@gmail.com>
2022-12-02 16:53:53 +0200

    1

--- Home_Work_4/Ex_1.py
+++ Home_Work_4/Ex_1.py
@@ -6,5 +6,22 @@
 
 from math import pi
 
-print(pi)
+#print(round(pi, 4))
 
+
+def calculate_Pi():
+    k = 1
+    x = float(0)
+    for k in range(1, 1000000):
+        x = float(x)+4*((-1)**(k+1))/(2*k-1)
+        #if result == round(pi, 4):
+        return x
+
+k = 1
+x = 0
+for k in range(1, 1000000):
+    x = x+4*((-1)**(k+1))/(2*k-1)
+print(x)
+
+
+print(calculate_Pi())
