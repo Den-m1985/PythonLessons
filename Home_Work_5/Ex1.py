2815d2f9bee1f5136e550d552425915fd2a3a5b9
den-m1985 <denismartyanov244@gmail.com>
2022-12-07 15:33:08 +0200

    finish first task

--- Home_Work_5/Ex1.py
+++ Home_Work_5/Ex1.py
@@ -1,28 +1,29 @@
 '''
 1 Напишите программу, удаляющую из файла все слова, содержащие "абв".
+text = 'ловатмивами абв пипви апыи'
 '''
 
 
 def read_file(file):
-    with open(file, encoding="utf8") as data:
-        file_read = data.readlines()
+    with open(file,'r', encoding="utf8") as data:
+        file_read = str(data.read())
     return file_read
 
 
 def write_to_file(file_name, result):
-    with open(file_name, 'w') as f_obj:
-        f_obj.write(str(result))
+    with open(file_name, 'a', encoding='utf-8') as f_obj:
+        f_obj.write(result)
 
 
 def delete_abc(text, value):
-    result = " ".join(list(filter(lambda x: not value in x, text.split( ))))
+    result = " ".join(filter(lambda x: not value in x, text.split( )))
     return result
+
 
 delete_word = 'абв'
 read_txt = read_file('Wome_work_5_1.txt')
 print(read_txt)
 result = delete_abc(read_txt, delete_word)
-print(result)
 write_to_file('Wome_work_5_1.txt', result)
 print(result)
 
