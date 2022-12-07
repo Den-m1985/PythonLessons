3990d93c5ff67f498016e8d0e36c07bc1dbdcf1f
den-m1985 <denismartyanov244@gmail.com>
2022-12-07 14:28:23 +0200

    first decision

--- Home_Work_5/Ex5.py
+++ Home_Work_5/Ex5.py
@@ -6,3 +6,18 @@
 
 [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
 '''
+lst = [1, 5, 2, 3, 4, 6, 1, 7]
+
+
+def consecutive_numbers(lst):
+    
+    for i in range(len(lst)):
+        arr_n = [1]
+        arr_n[0] = lst[0]
+        for j in range(i, len(lst)):
+            if lst[j] > arr_n[-1]:
+                arr_n.append(lst[j])
+        print(arr_n)
+        
+
+consecutive_numbers(lst)
