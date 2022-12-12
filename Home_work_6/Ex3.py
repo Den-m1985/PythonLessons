0583b006e3e8034d6c41a9cfb56f9283d20da591
den-m1985 <denismartyanov244@gmail.com>
2022-12-12 16:54:48 +0200

    1

--- Home_work_6/Ex3.py
+++ Home_work_6/Ex3.py
@@ -7,3 +7,32 @@
 Начиная с 3 семинара.
 '''
 
+# Домашка № 4 Перездача по list comprehension (строка 29) и добавил улучшение в строке 24
+
+'''
+Задайте последовательность чисел. Напишите программу,
+которая выведет список неповторяющихся элементов исходной последовательности.
+[1,1,1,2,2,3,5,4,4,7,8,8,7] -> [3,5]
+'''
+from random import randint
+
+
+def set_of_numbers(n):
+    #arr = [0] * n
+    #for i in range(n):
+    #    arr[i] = int(random() * 10)
+    arr = [randint(0, 9) for _ in range(6)]
+    return arr
+
+
+def sort(lst):
+    return [a for a in set(lst) if lst.count(a) < 2]
+
+
+n = 10  # Lenght list
+array = set_of_numbers(n)
+print(f'Исходный список: {array}')
+
+sort_list = sort(array)
+print(f'Список уникальных цисел: {sort_list}')
+
