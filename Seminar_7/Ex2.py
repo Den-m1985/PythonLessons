a5ee9eb75c74c58fc37c9c80a1b981b9c41041f9
den-m1985 <denismartyanov244@gmail.com>
2022-12-12 21:03:20 +0200

    2

--- Seminar_7/Ex2.py
+++ Seminar_7/Ex2.py
@@ -97,3 +97,15 @@ def ask_operation():
     operation = input(message) 
  
     return operation
+
+
+
+    operations = {'+': lambda x, y: x + y,
+              '-': lambda x, y: x - y,
+              '*': lambda x, y: x * y,
+              '/': lambda x, y: x / y}
+
+op = '+'
+n1, n2 = 25, 6
+
+print(operations[op](n1, n2))
