3866f0747ae901e57b50915915885e3e21efcc05
den-m1985 <denismartyanov244@gmail.com>
2022-12-23 12:37:23 +0200

    work tic tac toe

--- Home_work_9/tic_tac_toe_game.py
+++ Home_work_9/tic_tac_toe_game.py
@@ -3,50 +3,39 @@ import emoji
 # pip install emoji
 # tic tac toe game
 # 1. Создайте программу для игры в ""Крестики-нолики"" при помощи виртуального окружения и PIP
-
-
-print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
-#:skull:
-#:Christmas_tree:
-#:Santa_Claus:
+# :skull:
+# :Christmas_tree:
+# :Santa_Claus:
 # :alien:
 # https://carpedm20.github.io/emoji/    берем emoji от сюда
 
-square = {'1': emoji.emojize(':skull:'), '2': emoji.emojize(':skull:'), '3': emoji.emojize(':skull:\n'),
-        '4': emoji.emojize(':skull:'), '5': emoji.emojize(':skull:'), '6': emoji.emojize(':skull:\n'), 
-        '7': emoji.emojize(':skull:'), '8': emoji.emojize(':skull:'), '9': emoji.emojize(':skull:\n')}
-square_copy = dict.copy(square) # копия словаря
-square_copy ['1'] = emoji.emojize(':alien:')
-for key, value in square_copy.items():
-    
-    print(value, end='')
-print(emoji.emojize(':Santa_Claus:'))
+print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
+
+base_emoji = emoji.emojize(':skull:')
+smile_first_player = emoji.emojize(':Christmas_tree:')
+smile_second_player = emoji.emojize(':Santa_Claus:')
+square = {i: base_emoji for i in range(1, 10)}
+
 
 # рисует игровое поле
 def draw_board(square):
-    #print("-" * 13)
-    #for i in range(3):
-    #    print("|", square[0+i*3], "|", square[1+i*3], "|", square[2+i*3], "|")
-    #    print("-" * 13)
-        for key, value in square.items():
-            print(value, end='')
+    for row in range(1, 9, 3):
+        print('  '.join(square[key] for key in range(row, row + 3)))
 
 
 # принимает ввод пользователя. Проверяет корректность ввода
-def take_input(player_step):
+def take_input(smile):
     valid = False
     while not valid:
-        player_answer = input("Куда поставим " + player_step+"? ")
+        player_answer = input("Куда поставим " + smile+"? ")
         try:
             player_answer = int(player_answer)
         except:
             print("Некорректный ввод. Вы уверены, что ввели число?")
             continue
         if player_answer >= 1 and player_answer <= 9:
-            #if (str(square[player_answer-1]) not in "XO"):
-            if (str(square[player_answer-1]) not in "XO"):  # здесь переделать условие в словаре
-                #square[player_answer-1] = player_step
-                square[player_answer-1] = player_step   # здесь переделать замену в словаре на х
+            if square[player_answer] in base_emoji:
+                square[player_answer] = smile
                 valid = True
             else:
                 print("Эта клетка уже занята!")
@@ -56,11 +45,25 @@ def take_input(player_step):
 
 # функция проверки игрового поля, проверяет, выиграл ли игрок
 def check_win(square):
-    win_line = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),    # здесь проставить ключи словаря
-                (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    for each in win_line:
-        if square[each[0]] == square[each[1]] == square[each[2]]:
-            return square[each[0]]
+    #  проверка по строкам
+    if square[1] == square[2] and square[2] == square[3]:
+        return square[1]
+    elif square[4] == square[5] and square[5] == square[6]:
+        return square[4]
+    elif square[7] == square[8] and square[8] == square[9]:
+        return square[7]
+    #  проверка по столбам
+    elif square[1] == square[4] and square[4] == square[7]:
+        return square[1]
+    elif square[2] == square[5] and square[5] == square[8]:
+        return square[2]
+    elif square[3] == square[6] and square[6] == square[9]:
+        return square[3]
+    #  проверка по диагонали
+    elif square[1] == square[5] and square[5] == square[9]:
+        return square[1]
+    elif square[3] == square[5] and square[5] == square[7]:
+        return square[3]
     return False
 
 
@@ -69,36 +72,26 @@ def main(square):
     count = 0
     win = False
     while not win:
-        draw_board(square)  # работат
+        draw_board(square)
         if count % 2 == 0:
-            take_input("X")
+            take_input(smile_first_player)
         else:
-            take_input("O")
+            take_input(smile_second_player)
         count += 1
         if count > 4:
             tmp = check_win(square)
             if tmp:
-                print(tmp, "выиграл!")
+                print()
+                print(tmp, "выиграл!\n")
                 win = True
                 break
         if count == 9:
-            print("Ничья!")
+            print()
+            print("Ничья!\n")
             break
     draw_board(square)
 
 
-#main(square)
+main(square)
 
 input("Нажмите Enter для выхода!")
-
-'''
-print(emoji.emojize(':thumbs_up:'))
-desc = {1: emoji.emojize(':thumbs_up:'), "2": emoji.emojize(':thumbs_up:'), 
-        "3": emoji.emojize(':thumbs_up:\n'), "4": emoji.emojize(':thumbs_up:'), 
-        "5": emoji.emojize(':thumbs_up:'), "6": emoji.emojize(':thumbs_up:\n'), 
-        "7": emoji.emojize(':thumbs_up:'), "8": emoji.emojize(':thumbs_up:'), 
-        "9": emoji.emojize(':thumbs_up:')}
-
-for key, value in desc.items():
-    print(value, end='')
-'''
