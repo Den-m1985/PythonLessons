a55a6b9dd544bee5c269a947f9f3a30ce9a7701f
den-m1985 <denismartyanov244@gmail.com>
2022-12-25 09:40:29 +0200

    Бот принимает файлы

--- Home_work_9/Adress_book/main.py
+++ Home_work_9/Adress_book/main.py
@@ -1,9 +1,45 @@
-
+import os
+from telebot import TeleBot, types
+import messages as m
+import datetime
+os.chdir(os.path.dirname(__file__))
 
 '''
 Прикрутить бота к задачам с предыдущего семинара: 
     2.2Создать телефонный справочник с возможностью 
     импорта и экспорта данных в нескольких форматах.
+python -m pip install pytelegrambotapi
 '''
 
 
+token = open("token.config", "r").read() # прочитай README или config.txt
+bot = TeleBot(token)
+#TOKEN = '' 
+#bot = TeleBot(TOKEN)
+
+# команды Start и Info
+@bot.message_handler(commands=m.USER_COMMANDS)
+def send_welcome(message):
+    cmd = message.text.lstrip('/')
+    if cmd == 'start':
+        bot.reply_to(message, m.OPERATIONS)
+    else:
+        bot.reply_to(message, m.INFO_MESSAGE)
+
+
+# Функция для сохранения документа, отправленного боту
+@bot.message_handler(content_types=['document'])
+def answer(msg: types.Message):
+    filename = msg.document.file_name
+    with open(filename, 'wb') as file:
+        file.write(bot.download_file(
+            bot.get_file(msg.document.file_id).file_path))
+    bot.send_message(chat_id=msg.from_user.id, text=m.MESSAGE_1)
+
+    # Можете раскомментировать, если потребуется затем удалять файл после обработки,
+    # чтобы не тратить память.
+    # Не забудьте импортировать os
+    #os.remove(filename)
+
+
+bot.polling()
