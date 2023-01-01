ca3d9ca3336b3660202f78824fc190eb3cf5b818
den-m1985 <denismartyanov244@gmail.com>
2023-01-01 12:58:09 +0200

    рабочий вариант калькулятора

--- Home_work_9/Calculator/main.py
+++ Home_work_9/Calculator/main.py
@@ -1,8 +1,7 @@
 import os
 from telebot import TeleBot
 import messages as m
-import datetime
-import codecs
+import Loging
 os.chdir(os.path.dirname(__file__))
 
 '''
@@ -25,83 +24,75 @@ def send_welcome(message):
         bot.reply_to(message, m.OPERATIONS + m.MESSAGE_2 + m.MESSAGE_3)
     else:
         bot.reply_to(message, m.INFO_MESSAGE)
+    Loging.log(message)
 
 
 @bot.message_handler()
 def send_welcome(message):
-
-    #text = message.text
     text = message.text.split()
-    if text[1] == '+':
-        bot.reply_to(
-            message, text=f'Результат сложения {float(text[0]) + float(text[2])}')
-        bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    elif text[1] == '-':
-        bot.reply_to(
-            message, text=f'Результат вычитания {float(text[0]) - float(text[2])}')
-        bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    elif text[1] == '/':
-        if text[2] == '0':
-            bot.reply_to(message, text='деление на 0 не допустимо')
+    if len(text) == 3:
+        if text[1] == '+':
+            answer = float(text[0]) + float(text[2])
+            bot.reply_to(message, text=f'Результат сложения {answer}')
             bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
+        elif text[1] == '-':
+            answer = float(text[0]) - float(text[2])
+            bot.reply_to(message, text=f'Результат вычитания {answer}')
+            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
+        elif text[1] == '/':
+            if text[2] == '0':
+                answer = 'eror'
+                bot.reply_to(message, text='деление на 0 не допустимо')
+                bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+            else:
+                answer = float(text[0]) / float(text[2])
+                bot.reply_to(message, text=f'Результат деления {answer}')
+                bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
+        elif text[1] == '*':
+            answer = float(text[0]) * float(text[2])
+            bot.reply_to(message, text=f'Результат умножения {answer}')
+            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
+        elif text[1] == '++':
+            answer = complex(text[0]) + complex(text[2])
+            bot.reply_to(message, text=f'Результат сложения {answer}')
+            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
+        elif text[1] == '--':
+            answer = complex(text[0]) + complex(text[2])
+            bot.reply_to(message, text=f'Результат вычитания {answer}')
+            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
+        elif text[1] == '—':
+            answer = 'eror'
+            bot.send_message(chat_id=message.from_user.id, text=m.MESSAGE_4)
+
+        elif text[1] == '//':
+            if text[2] == '0':
+                answer = 'eror'
+                bot.reply_to(message, text='деление на 0 не допустимо')
+                bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+            else:
+                answer = complex(text[0]) / complex(text[2])
+                bot.reply_to(message, text=f'Результат деления {answer}')
+                bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
+        elif text[1] == '**':
+            answer = complex(text[0]) * complex(text[2])
+            bot.reply_to(message, text=f'Результат умножения {answer}')
+            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
+
         else:
-            bot.reply_to(
-                message, text=f'Результат деления {float(text[0]) / float(text[2])}')
-            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    elif text[1] == '*':
-        bot.reply_to(
-            message, text=f'Результат умножения {float(text[0]) * float(text[2])}')
-        bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    elif text[1] == '++':
-        bot.reply_to(
-            message, text=f'Результат сложения {complex(text[0]) + complex(text[2])}')
-        bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    elif text[1] == '--':
-        bot.reply_to(
-            message, text=f'Результат вычитания {complex(text[0]) - complex(text[2])}')
-        bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    elif text[1] == '—':
-        bot.send_message(chat_id=message.from_user.id, text=m.MESSAGE_4)
-
-    elif text[1] == '//':
-        if text[2] == '0':
-            bot.reply_to(message, text='деление на 0 не допустимо')
-            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-        else:
-            bot.reply_to(message, text=f'Результат деления {complex(text[0]) / complex(text[2])}')
-            bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    elif text[1] == '**':
-        bot.reply_to(
-            message, text=f'Результат умножения {complex(text[0]) * complex(text[2])}')
-        bot.send_message(chat_id=message.from_user.id,
-                         text=m.MESSAGE_2 + m.MESSAGE_3)
-
-    else:
-        bot.reply_to(message, text=m.MESSAGE_1)
+            answer = 'eror'
+            bot.reply_to(message, text=m.MESSAGE_1)
+    else:  
+        answer = 'eror'
+        bot.send_message(chat_id=message.from_user.id,text=m.MESSAGE_1 + m.MESSAGE_3)
     
-    log(message, text)
+    Loging.log(message, answer)
     
-
-def log(message, text):
-    dtn = datetime.datetime.now()
-    # логирование в 'log_calculator.txt'
-    botlogfile = open('log_calculator.txt', 'a', encoding='utf-8')
-    print(dtn.strftime("%d-%m-%Y %H:%M"), 'Пользователь ' +
-          message.from_user.first_name, message.from_user.id,
-          'операция: ' + ' '.join(text), file=botlogfile)
-    botlogfile.close()
-    
-    # логирование в 'log'
-    with codecs.open('log', 'a', encoding='utf-8') as file:
-        file.writelines(
-            f'\n Chat {message.chat.id} User: {message.from_user.first_name} Data: {dtn} Message: {message.text}')
-
 
 bot.polling(none_stop=True, interval=0)
